c08d37da22206e7c0ab0ea902d2aba582568be5f
raphael.bernardo <raphael.bernardo@concrete.com.br>
2018-09-06 16:44:05 -0300

    Adiciona lib do MQTT da fusesource

--- src/br/edu/infnet/raphaelbgr/lightcontrol/MainApplication.java
+++ src/br/edu/infnet/raphaelbgr/lightcontrol/MainApplication.java
@@ -1,8 +1,20 @@
 package br.edu.infnet.raphaelbgr.lightcontrol;
 
+import org.fusesource.mqtt.client.MQTT;
+import org.fusesource.mqtt.client.QoS;
+
+import java.net.URISyntaxException;
+
 public class MainApplication {
 
     public static void main(String[] args) {
-        
+        try {
+            MQTT mqtt = new MQTT();
+            mqtt.setKeepAlive((short) 12000);
+            mqtt.setHost("tcp://iot.eclipse.org:1883");
+            mqtt.setWillQos(QoS.AT_LEAST_ONCE);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
