c235ed636aa48d045689993738c95f4ac42b453c
Raphael Bernardo <raphaelbgr@gmail.com>
2018-09-19 22:26:13 -0300

    Cria a base de MQTT java do backend do raspi 3, faz conexão com o iot.eclipse.org

--- src/br/edu/infnet/raphaelbgr/lightcontrol/MainApplication.java
+++ src/br/edu/infnet/raphaelbgr/lightcontrol/MainApplication.java
@@ -1,20 +1,122 @@
 package br.edu.infnet.raphaelbgr.lightcontrol;
 
-import org.fusesource.mqtt.client.MQTT;
-import org.fusesource.mqtt.client.QoS;
+import org.fusesource.hawtbuf.Buffer;
+import org.fusesource.hawtbuf.UTF8Buffer;
+import org.fusesource.mqtt.client.*;
 
 import java.net.URISyntaxException;
 
 public class MainApplication {
 
+    private static MQTT mqtt = new MQTT();
+    private static CallbackConnection connection;
+
     public static void main(String[] args) {
+        System.out.println("Server> Program start...");
+        setupMqttClient();
+        createMqttCallbackConnection();
+        connectMqtt();
+
+        slowDownPolicy();
+    }
+
+    private static void slowDownPolicy() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void setupMqttClient() {
         try {
-            MQTT mqtt = new MQTT();
-            mqtt.setKeepAlive((short) 12000);
             mqtt.setHost("tcp://iot.eclipse.org:1883");
             mqtt.setWillQos(QoS.AT_LEAST_ONCE);
+            connection = mqtt.callbackConnection();
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void createMqttCallbackConnection() {
+        connection.listener(new Listener() {
+            @Override
+            public void onConnected() {
+                System.out.println("MQQT> Connected to iot.eclipse.org!");
+            }
+
+            @Override
+            public void onDisconnected() {
+                System.out.println("MQQT> Disconnected socket...");
+            }
+
+            @Override
+            public void onPublish(UTF8Buffer utf8Buffer, Buffer buffer, Runnable runnable) {
+                runnable.run();
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println("MQQT> Failure on socket...");
+            }
+        });
+    }
+
+    private static void connectMqtt() {
+        connection.connect(new Callback<Void>() {
+            public void onFailure(Throwable value) {
+                System.out.println("MQQT> Socket breakdown...");
+            }
+
+            public void onSuccess(Void v) {
+                System.out.println("MQQT> Connected socket!");
+                subscribeToTopic();
+            }
+        });
+    }
+
+    private static void subscribeToTopic() {
+        Topic[] topics = {new Topic("tcc_light_control_infnet", QoS.AT_LEAST_ONCE)};
+        connection.subscribe(topics, new Callback<byte[]>() {
+            public void onSuccess(byte[] qoses) {
+                // The result of the subcribe request.
+                System.out.println("MQQT> Subscribed succesfully to topic tcc_light_control_infnet");
+                publishImAlive();
+            }
+
+            public void onFailure(Throwable value) {
+                System.out.println("MQQT> Failure to subscribe on topic.");
+            }
+        });
+    }
+
+    private static void publishImAlive() {
+        // Send a message to a topic
+        connection.publish("tcc_light_control_infnet", "raspberry_pi_im_alive".getBytes(), QoS.AT_LEAST_ONCE, false, new Callback<Void>() {
+            public void onSuccess(Void v) {
+                // the pubish operation completed successfully.
+                System.out.println("MQQT> Published to topic.");
+            }
+
+            public void onFailure(Throwable value) {
+                System.out.println("MQQT> Failure to publish on topic.");
+            }
+        });
+    }
+
+    private void disconnect() {
+        // To disconnect..
+        connection.disconnect(new Callback<Void>() {
+            public void onSuccess(Void v) {
+                // called once the connection is disconnected.
+                System.out.println("MQQT> Disconnected.");
+            }
+
+            public void onFailure(Throwable value) {
+                // Disconnects never fail.
+            }
+        });
     }
 }
