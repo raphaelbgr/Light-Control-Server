20c467bd5f46679deb878538287b6a2b83c824f8
Raphael Bernardo <raphaelbgr@gmail.com>
2020-06-23 16:46:35 -0300

    + improved connectivity check

--- src/main/java/br/edu/infnet/raphaelbgr/lightcontrol/util/Util.java
+++ src/main/java/br/edu/infnet/raphaelbgr/lightcontrol/util/Util.java
@@ -7,45 +7,58 @@ import java.net.URLConnection;
 public class Util {
 
     public static boolean isReachableByPing(String host) {
-        try {
-            String cmd;
-            // For Linux and OSX
-            if (System.getProperty("os.name").startsWith("Windows")) {
-                // For Windows
-                cmd = "ping -n 1 " + host;
-            } else cmd = "ping -c 1 " + host;
+        for (int i = 0; i <= 5; i++) {
+            try {
+                String cmd;
+                // For Linux and OSX
+                if (System.getProperty("os.name").startsWith("Windows")) {
+                    // For Windows
+                    cmd = "ping -n 1 " + host;
+                } else cmd = "ping -c 1 " + host;
 
-            Process myProcess = Runtime.getRuntime().exec(cmd);
-            myProcess.waitFor();
+                Process myProcess = Runtime.getRuntime().exec(cmd);
+                myProcess.waitFor();
 
-            if (myProcess.exitValue() == 0) {
+                if (myProcess.exitValue() == 0) {
 //                System.out.println("SERVER> Ping to " + host + " SUCCESS");
-                return true;
-            } else {
-                System.err.println("SERVER> Ping to " + host + " FAILURE");
-                return false;
-            }
+                    return true;
+                } else {
+                    System.err.println("SERVER> Ping to " + host + " FAILURE");
+                }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            waitOneSec();
         }
+        return false;
     }
 
     public static boolean isReachableByHttp(String host) {
-        try {
-            URL url = new URL(host);
-            URLConnection connection = url.openConnection();
+        for (int i = 0; i <= 5; i++) {
+            try {
+                URL url = new URL(host);
+                URLConnection connection = url.openConnection();
 
-            if (connection.getContentLength() == -1) {
-                System.err.println("SERVER> URLConnection failed to host: " +  host);
-                return false;
+                if (connection.getContentLength() == -1) {
+                    System.err.println("SERVER> URLConnection failed to host: " + host);
+                    return false;
+                }
+                return true;
+            } catch (IOException e) {
+                System.err.println("SERVER> IOException to host: " + host);
+                e.printStackTrace();
             }
-            return true;
-        } catch (IOException e) {
-            System.err.println("SERVER> IOException to host: " +  host);
+            waitOneSec();
+        }
+        return false;
+    }
+
+    private static void waitOneSec() {
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
             e.printStackTrace();
-            return false;
         }
     }
 }
